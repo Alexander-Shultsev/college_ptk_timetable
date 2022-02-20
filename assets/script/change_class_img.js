ae4965caac30f5a1d66f69cb5240968a80636068
Студент ПТК <student@ptk.ru>
2022-02-20 18:57:40 +0300

    1

--- assets/script/change_class_img.js
+++ assets/script/change_class_img.js
@@ -2,11 +2,17 @@ var allButtons = document.querySelectorAll('button.class__btn');
 var allClass = document.querySelectorAll('div.timetable__class');
 
 let current_class = allClass[0];
+let current_button = allButtons[0];
 
 for(let i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener('click', function(e) {
         current_class.classList.remove('show');
         allClass[i].classList.add('show');
+
+        current_button.classList.remove('class__btn--active');
+        allButtons[i].classList.add('class__btn--active');
+
         current_class = allClass[i];
+        current_button = allButtons[i];
     })
 }
